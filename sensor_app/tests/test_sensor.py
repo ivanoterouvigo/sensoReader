6273b10cf7df3835cf455cfe29c2aa8b7b25151d
Iván Otero <ivanoterouvigo@gmail.com>
2024-07-04 00:15:12 +0200

    Create test_sensor.py
    test sensor

---
+++ sensor_app/tests/test_sensor.py
@@ -0,0 +1,14 @@
+import unittest
+from src.sensor import Sensor
+
+class TestSensor(unittest.TestCase):
+
+    def test_read_data_mockup(self):
+        sensor = Sensor(sensor_type='mockup', range_min=0, range_max=65535)
+        data = sensor.read_data()
+        self.assertEqual(len(data), 64)
+        for value in data:
+            self.assertTrue(0 <= value <= 65535)
+
+if __name__ == '__main__':
+    unittest.main()
