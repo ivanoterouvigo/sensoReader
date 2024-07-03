9b10585b78821b78e41bcf98bddfa619bcc62d9e
Iván Otero <ivanoterouvigo@gmail.com>
2024-07-04 00:57:29 +0200

    Update main.py
    comments to clarify

--- sensor_app/main.py
+++ sensor_app/main.py
@@ -11,18 +11,23 @@ logging.basicConfig(level=logging.INFO)
 async def main():
     args = parse_arguments()
     
+    # Sensor instance
     sensor = Sensor(sensor_type=args.sensor_type, range_min=args.range_min, range_max=args.range_max)
+    # DDBB instance by URI
     db = Database(db_uri=args.db_uri)
     nats_client = NATSClient()
 
+    # Connect to server NATS client
     await nats_client.connect()
 
+    # Async function to catch data sensor
     async def capture_data():
         while True:
             data = sensor.read_data()
             db.insert_data(pickle.dumps(data))
             await nats_client.publish("sensor.data", pickle.dumps(data))
             logging.info(f"Data captured and published: {data}")
+            # Interval time checking before data capture
             await asyncio.sleep(args.interval)
 
     async def start_capture():
