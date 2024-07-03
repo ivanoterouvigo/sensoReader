ba0d2c5860c57cfe3f5f36108af523cd6aca41ff
Iván Otero <ivanoterouvigo@gmail.com>
2024-07-04 00:32:16 +0200

    Update test_sensor.py
    added comments

--- sensor_app/tests/test_sensor.py
+++ sensor_app/tests/test_sensor.py
@@ -1,13 +1,20 @@
 import unittest
 from sensor import Sensor
 
+# Class defined from unittest.TestCase
 class TestSensor(unittest.TestCase):
 
     def test_read_data_mockup(self):
+        # Sensor instance with min, max
         sensor = Sensor(sensor_type='mockup', range_min=0, range_max=65535)
+        
+        # read_data from sensor
         data = sensor.read_data()
+        
+        # Check data len = 64
         self.assertEqual(len(data), 64)
         for value in data:
+            # Check value inside (0 to 65535)
             self.assertTrue(0 <= value <= 65535)
 
 if __name__ == '__main__':
